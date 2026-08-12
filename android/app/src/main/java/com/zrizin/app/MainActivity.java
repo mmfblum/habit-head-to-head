32ea1e8833e729b37bf5ff95bdb0afdc7f3db725
mmfblum <mmfblum@gmail.com>
2026-08-12 16:04:09 -0400

    Register Android Zrizin health plugin

--- android/app/src/main/java/com/zrizin/app/MainActivity.java
+++ android/app/src/main/java/com/zrizin/app/MainActivity.java
@@ -1,5 +1,12 @@
 package com.zrizin.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(ZrizinHealthPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
